850cf9303474395fa3d5962b9d4e70802983a481
nteferra <123516477+nteferra@users.noreply.github.com>
2023-05-05 13:19:48 -0400

    add multiple elements and functions
    add date function, local storage manipulation, styles, and hr

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const ulEl = document.getElementById('ul-el')
 const inputEl = document.getElementById('input-el')
 const buttonEl = document.getElementById('button-el')
+const deleteBtn = document.getElementById('delete-btn')
+const tasksFromLocalStorage = JSON.parse(localStorage.getItem("myTasks"))
 let myTasks = []
+let date = document.querySelector('.date')
+date.textContent = Date()
+
+if (tasksFromLocalStorage) {
+    myTasks = tasksFromLocalStorage
+    addTask(myTasks)
+}
 
 function addTask(tasks) {
     let listItems = ''
@@ -11,10 +20,19 @@ function addTask(tasks) {
     ulEl.innerHTML = listItems
 }
 
+
+
 buttonEl.addEventListener('click', () => {
     myTasks.push(inputEl.value)
     inputEl.value = '';
+    localStorage.setItem("myTasks", JSON.stringify(myTasks) )
     addTask(myTasks)
     
 
+})
+
+deleteBtn.addEventListener('click', () => {
+    localStorage.clear()
+    myTasks = []
+    addTask(myTasks)
 })
