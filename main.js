dcaa8f95de612a11f3d3f5556fa7b11fce7110b2
nteferra <123516477+nteferra@users.noreply.github.com>
2023-05-15 14:36:13 -0400

    update delete button function and style

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const ulEl = document.getElementById('ul-el')
 const inputEl = document.getElementById('input-el')
 const buttonEl = document.getElementById('button-el')
-const deleteBtn = document.getElementById('delete-btn')
+const clearBtn = document.getElementById('clear-btn')
 const tasksFromLocalStorage = JSON.parse(localStorage.getItem("myTasks"))
 let myTasks = []
 let date = document.querySelector('.date')
 date.textContent = Date().substring(0, Date().length - 41)
-let clearBtn = document.getElementsByClassName('clear')
 
 
 if (tasksFromLocalStorage) {
@@ -17,7 +16,7 @@ if (tasksFromLocalStorage) {
 function addTask(tasks) {
     let listItems = ''
     for (let i = 0; i < tasks.length; i++) {
-        listItems += `<li> > ${tasks[i]}<button class="clear">x</button></li>`;
+        listItems += `<li> > ${tasks[i]}<button class="del" onclick="this.parentElement.remove()">X</button></li>`;
     }
     ulEl.innerHTML = listItems
 }
@@ -34,7 +33,7 @@ buttonEl.addEventListener('click', () => {
 
 })
 
-deleteBtn.addEventListener('click', () => {
+clearBtn.addEventListener('click', () => {
     localStorage.clear()
     myTasks = []
     addTask(myTasks)
